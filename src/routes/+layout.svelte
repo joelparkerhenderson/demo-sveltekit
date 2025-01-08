8e3042bf41fcf03a4761c39df0f68d648b8885c1
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-01-08 07:23:29 -0800

    Add layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,13 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import '../app.css';
+    import Header from "$lib/Header.svelte";
+    import Footer from "$lib/Footer.svelte";
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
+    <Header />
 	{@render children()}
+    <Footer />
 </ParaglideJS>
