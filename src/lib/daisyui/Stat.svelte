528d7f8bbf92c442749ca456e8b9ab3075bbeb37
Joel Parker Henderson <joel@joelparkerhenderson.com>
2025-02-10 20:05:17 -0800

    Add daisyUI Stat

---
+++ src/lib/daisyui/Stat.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { title, value, description } = $props();
+</script>
+
+<div class="stat">
+    <div class="stat-title">{title}</div>
+    <div class="stat-value text-primary">{value}</div>
+    <div class="stat-desc">{description}</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import Stat from '$lib/daisyui/Stat.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Stat title="My Title" value="My Value" description="My Description" />
